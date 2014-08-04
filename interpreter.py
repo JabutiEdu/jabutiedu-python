40cd2e53dfb9522153742c3ba9d4e4ee0d4189c6
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2014-08-04 16:29:38 -0300

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -5,6 +5,11 @@ import sys
 import RPi.GPIO as GPIO
 import time
 
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
@@ -30,49 +35,67 @@ def desligar():
     GPIO.cleanup()
 
 message = sys.argv[1]
-delay = int(sys.argv[2])
+try:
+    timeout = float(sys.argv[2])
+except ValueError:
+    saying = " "
+    parsed = sys.argv[2:]
+    for word in parsed:
+        saying = saying + word
+    print saying
+print "executando "
+print message
+print time
+
 if message == 'pf':
-    GPIO.output(24, GPIO.HIGH)
-    GPIO.output(23, GPIO.LOW)
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(22, GPIO.LOW)
-    time.sleep(delay)
-    parar()
-elif message == 'pt':
+
     GPIO.output(24, GPIO.LOW)
     GPIO.output(23, GPIO.HIGH)
     GPIO.output(22, GPIO.HIGH)
     GPIO.output(17, GPIO.LOW)
-    time.sleep(delay)
+
+    time.sleep(timeout)
+    parar()
+elif message == 'pt':
+
+    GPIO.output(24, GPIO.HIGH)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(22, GPIO.LOW)    
+    time.sleep(timeout)
     parar()
 elif message == 'pe':
     GPIO.output(23, GPIO.LOW)
     GPIO.output(24, GPIO.HIGH)
     GPIO.output(22, GPIO.HIGH)
     GPIO.output(17, GPIO.LOW)
-    time.sleep(delay)
+    timeout = timeout / 100
+    time.sleep(timeout)
     parar()
 elif message == 'lde':
     GPIO.output(9, GPIO.HIGH)
     GPIO.output(11, GPIO.HIGH)
-    time.sleep(delay)
+    time.sleep(timeout)
     desligar()
 elif message == 'ld':
     GPIO.output(9, GPIO.HIGH)
-    time.sleep(delay)
+    time.sleep(timeout)
     desligar()
 elif message == 'le':
     GPIO.output(11, GPIO.HIGH)
-    time.sleep(delay)
+    time.sleep(timeout)
+    desligar()
+elif message == 'off':
     desligar()
 elif message == 'pd':
     GPIO.output(24, GPIO.LOW)
     GPIO.output(23, GPIO.HIGH)
     GPIO.output(17, GPIO.HIGH)
     GPIO.output(22, GPIO.LOW)
-    time.sleep(delay)
+    timeout = timeout / 100
+    time.sleep(timeout)
     parar()
-elif message == 'som':
-    os.system("espeak \"oi, eu sou a jabuti edu\" -v portugal -s140 -p60 -g2 -a100")
-elif message == 'musica':
-    os.system("mpg321 /home/pi/musica.mp3")
+elif message == 'sm':
+    os.system('espeak "' + saying + '" -v brazil -s130 -p60 -g2 -a100')
+elif message == 'bu':
+    os.system("mpg321 /home/pi/future.mp3")
